3fb82e50c52fe473096c0b1dc896fefe47655b96
huynonstop <tthuykh99@gmail.com>
2020-08-18 00:49:41 +0700

    setup dep, eslint

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+app.use(bodyParser.json());
+app.use(cors());
+if (app.get('env') === 'development') {
+  app.use(morgan('combined'));
+}
+
+app.get('/test', (req, res) => {
+  res.status(200).json({
+    message: 'Success',
+  });
+});
+app.use('*', (req, res) => {
+  res.status(404).json({
+    message: 'Not Found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || 'Something went wrong!',
+  });
+  next();
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, async () => {
+  try {
+    console.log('Database connected!');
+    console.log('Redis connected!');
+    console.log(`Server is listening on port ${port}!`);
+  } catch (error) {
+    console.log(error);
+  }
+});
